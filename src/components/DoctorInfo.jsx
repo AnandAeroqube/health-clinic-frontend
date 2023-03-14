8948b906c163620573ea3ef9ae4d5090dc9d904e
AnandAeroqube <119660983+AnandAeroqube@users.noreply.github.com>
2023-03-14 19:40:20 +0530

    Added Doctor Info Card

---
+++ src/components/DoctorInfo.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import '../assets/styles/doctor-info.css'
+
+function DoctorInfo() {
+  return (
+    <div class="card">
+  <div class="card-body">
+    <h3 class="card-title">Dr Jena Doe</h3>
+    <h5 className='h5-div'>Dentist</h5>
+    <p class="card-text1">12 years of experience.</p>
+    <h5 className="h5-css">
+        Park Street Canada
+    </h5>
+    <p>John Digital Clinic +1more</p>
+  </div>
+</div>
+  )
+}
+
+export default DoctorInfo
