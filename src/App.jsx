67ad725ecb20e65e5fdefc0aa324f317b0963598
AnandAeroqube <119660983+AnandAeroqube@users.noreply.github.com>
2023-03-17 09:52:00 +0530

    Updated App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,39 +1,12 @@
-import React, {useEffect, useState} from "react";
-import {useSelector, useDispatch} from "react-redux";
-import {getDataFromApi} from "./redux/actions/fetchDataActions/fetchDataAction";
-import "./assets/styles/App.css";
-import logo from './assets/images/logo.svg';
-import Header from "./components/header";
-import Detail from "./components/Detail";
-import Footer from "./components/Footer";
-import LeftPane from "./components/LeftPane";
-import Account from "./components/Account";
+import React from "react";
+import {Routes, Route} from 'react-router-dom';
+import Home from './containers/Home';
 
-/**
- * functional component App.js
- */
 const App = () => {
-    const content = useSelector(state => state.main);
-    const dispatch = useDispatch();
-
-    /** equivalent to componentDidMount with second params as [] */
-    useEffect(() => {
-        dispatch(getDataFromApi());
-        console.log(content)
-    }, []);
-
-    const [detail, setDetail] = useState("My Account");
-
-    const updateDetail = (state) => {
-        setDetail(state);
-        console.log(state)
-    }
-
-
-    return (
-        <>
-            <div className="main-div">This is an app</div>
-        </>
-    );
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+    </Routes>
+  );
 };
 export default App;
